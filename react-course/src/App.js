c729bc87547da539954296a60ed7cd1525463d09
Alan Leverenz <awleverenz@aol.com>
2022-12-17 12:33:11 -0500

    updated App

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -2,7 +2,15 @@
 
 function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div>
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
