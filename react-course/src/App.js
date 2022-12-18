e36813a83fcb7edec3b0a08153413667b2ef20f8
Alan Leverenz <awleverenz@aol.com>
2022-12-18 15:52:38 -0500

    updated Todo component

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,15 +1,10 @@
-
+import Todo from './components/Todo'
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div>
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo />
     </div>
   );
 }
--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -1,5 +1,13 @@
 function Todo() {
-
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className='actions'>
+        <span>My span here</span>
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
