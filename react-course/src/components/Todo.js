ca808b22a032c7e073b7dd3fba9d09ef0a33e238
Alan Leverenz <awleverenz@aol.com>
2022-12-19 12:03:20 -0500

    applied closeModalHandler

--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -21,7 +21,7 @@ function Todo(props) {
       <div className='actions'>
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
-      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
       {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
