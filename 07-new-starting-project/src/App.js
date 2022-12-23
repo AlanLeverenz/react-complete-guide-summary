83cf49b95ce36dc3701fa1cb2e500d343318f9f2
Alan Leverenz <awleverenz@aol.com>
2022-12-22 23:22:45 -0500

    added MainNavigation component

--- 07-new-starting-project/src/App.js
+++ 07-new-starting-project/src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layouts/MainNavigation';
 
 function App() {
   // localhost:3000/
   // my-page.com/
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetupPage />} />
