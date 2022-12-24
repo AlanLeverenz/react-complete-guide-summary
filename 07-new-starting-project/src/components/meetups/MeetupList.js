2c4ab2d0b435efee2f69c4dff4707a593bce8609
Alan Leverenz <awleverenz@aol.com>
2022-12-24 14:33:24 -0500

    updated AllMeetups component

--- 07-new-starting-project/src/components/meetups/MeetupList.js
+++ 07-new-starting-project/src/components/meetups/MeetupList.js
@@ -4,7 +4,16 @@ import classes from './MeetupList.module.css';
 function MeetupList(props) {
   return (
     <ul className={classes.list}>
-      {props.meetups.map(meetup => <MeetupItem />)}
+      {props.meetups.map(meetup => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+          description={meetup.description}
+        />
+      ))}
     </ul>
   )
 }
