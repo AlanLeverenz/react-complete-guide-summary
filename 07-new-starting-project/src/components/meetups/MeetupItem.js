69714bd3f961df00272db61509412337be20adb6
Alan Leverenz <awleverenz@aol.com>
2022-12-24 13:50:19 -0500

    added MeetupItem

---
+++ 07-new-starting-project/src/components/meetups/MeetupItem.js
@@ -0,0 +1,16 @@
+function MeetUpItem() {
+  return (
+    <li>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h3>TITLE</h3>
+        <address>ADDRESS</address>
+        <p>DESCRIPTION</p>
+      </div>
+    </li>
+  )
+}
+
+export default MeetUpItem;
