62ec9ca456598d44db4ad599c197b4a83ef93295
Alan Leverenz <awleverenz@aol.com>
2022-12-26 11:43:17 -0500

    wrapped Layout around Routes

--- 07-new-starting-project/src/App.js
+++ 07-new-starting-project/src/App.js
@@ -3,20 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layouts/MainNavigation';
+import Layout from './components/layouts/Layout';
 
 function App() {
   // localhost:3000/
   // my-page.com/
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetupPage />} />
         <Route path='/favorites' element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
+
   );
 }
 
