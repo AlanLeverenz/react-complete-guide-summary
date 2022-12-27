340dbccbc60a400b2ec0874ce497a68de6fe2b54
Alan Leverenz <awleverenz@aol.com>
2022-12-27 15:36:47 -0500

    added MeetupForm

---
+++ 07-new-starting-project/src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,15 @@
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form>
+
+      </form>
+    </Card>
+  )
+}
+
+export default NewMeetupForm;
+
--- 07-new-starting-project/src/pages/NewMeetup.js
+++ 07-new-starting-project/src/pages/NewMeetup.js
@@ -1,6 +1,11 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
   return (
-    <div>New Meetup Page</div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
   );
 }
 
