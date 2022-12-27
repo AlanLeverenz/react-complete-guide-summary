7811d79a4f7e901beb114e0457ae68a4daa50efc
Alan Leverenz <awleverenz@aol.com>
2022-12-27 15:51:39 -0500

    updated form

--- 07-new-starting-project/src/components/meetups/NewMeetupForm.js
+++ 07-new-starting-project/src/components/meetups/NewMeetupForm.js
@@ -4,8 +4,26 @@ import classes from './NewMeetupForm.module.css';
 function NewMeetupForm() {
   return (
     <Card>
-      <form>
-
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Address</label>
+          <textarea required id="description" rows="5"></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
       </form>
     </Card>
   )
