0018c6a202f31a905dd93a7a86de704688910766
Alan Leverenz <awleverenz@aol.com>
2022-12-30 12:20:21 -0500

    added http post to Firebase

--- 07-new-starting-project/src/pages/NewMeetup.js
+++ 07-new-starting-project/src/pages/NewMeetup.js
@@ -3,6 +3,16 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
   function addMeetupHandler(meetupData) {
     // send http request to post meetup data
+    // fetch is a JavaScript built-in function
+    // Firebase requires JSON
+    fetch('https://react-getting-started-167ee-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
   }
 
   return (
