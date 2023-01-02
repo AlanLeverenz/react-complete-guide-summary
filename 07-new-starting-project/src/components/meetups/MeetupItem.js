c91e99d7ea39e27b9513ed6c940eb96f44d0cf69
Alan Leverenz <awleverenz@aol.com>
2023-01-02 16:55:40 -0500

    setup button for handling favorites

--- 07-new-starting-project/src/components/meetups/MeetupItem.js
+++ 07-new-starting-project/src/components/meetups/MeetupItem.js
@@ -1,7 +1,30 @@
+import { useContext } from 'react';
+// establishes a connection 
+
 import classes from './MeetupItem.module.css';
 import Card from '../ui/Card';
+import FavoritesContext from '../../store/favorites-context';
 
 function MeetUpItem(props) {
+  // gives this component access to the context content
+  const favoritesCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address
+      })
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -14,7 +37,8 @@ function MeetUpItem(props) {
           <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}</button>
         </div>
       </Card>
     </li>
