d5c7b18851613a10c5d41ceee4cfc9af1c13832e
Alan Leverenz <awleverenz@aol.com>
2023-01-01 21:41:13 -0500

    added functions for add, remove favorites

--- 07-new-starting-project/src/store/favorites-context.js
+++ 07-new-starting-project/src/store/favorites-context.js
@@ -7,10 +7,27 @@ const FavoritesContext = createContext({
 });
 
 // provides context to all interested components
-// and updates values
+// and updates values when state changes
 function FavoritesContextProvider(props) {
 
   const [userFavorites, setUserFavorites] = useState([]);
+
+  // guarantees latest state update, schedules functions in correct order
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup)
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites(prevUserFavorites => {
+      return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+    })
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  }
 
   const context = {
     favorites: userFavorites,
