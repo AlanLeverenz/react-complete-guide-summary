87b2dfdba027bb265b156aa9b73ac132cf4b032d
Alan Leverenz <awleverenz@aol.com>
2023-01-03 23:15:19 -0500

    added badge to MainNavigation

--- 07-new-starting-project/src/pages/Favorites.js
+++ 07-new-starting-project/src/pages/Favorites.js
@@ -4,12 +4,20 @@ import FavoritesContext from '../store/favorites-context';
 import MeetupList from '../components/meetups/MeetupList';
 
 function FavoritesPage() {
-  const favoritesCtx = useContext(FavoritesContext)
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You have no favorites yet. Start adding some?</p>
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />
+  }
 
   return (
     <section>
       <h1>My Favorites</h1>
-      <MeetupList meetups={favoritesCtx.favorites} />
+      {content}
     </section>
   );
 }
