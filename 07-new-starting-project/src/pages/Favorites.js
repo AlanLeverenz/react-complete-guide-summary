8b9115bfa56c371a92b0dd06e6bcbc13c4611e10
Alan Leverenz <awleverenz@aol.com>
2023-01-03 23:01:47 -0500

    add context to Favorites

--- 07-new-starting-project/src/pages/Favorites.js
+++ 07-new-starting-project/src/pages/Favorites.js
@@ -1,6 +1,16 @@
+import { useContext } from 'react';
+
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext)
+
   return (
-    <div>Favorites Page</div>
+    <section>
+      <h1>My Favorites</h1>
+      <MeetupList meetups={favoritesCtx.favorites} />
+    </section>
   );
 }
 
