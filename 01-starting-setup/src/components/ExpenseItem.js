ea1c089c9fb853e0ed9794ec57693d65fc49b3c7
Alan Leverenz <awleverenz@aol.com>
2023-01-09 06:49:44 -0600

    updated ExpenseItem

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,11 @@
 function ExpenseItem() {
   return (
-    <h2>Expense Item</h2>
+    <div>
+      <div>Date</div>
+      <div><h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
   )
 }
 
