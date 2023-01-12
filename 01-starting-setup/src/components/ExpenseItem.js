d5c71ecb7a1a29b17ffec4a66add945dc6d2dff4
Alan Leverenz <awleverenz@aol.com>
2023-01-11 22:35:20 -0600

    added day and year const

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -2,14 +2,16 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
-  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
       <div>
         <div>{month}</div>
-        <div>Year</div>
-        <div>Date</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
       <div>{props.Date}</div>
       <div className="expense-item__description">
