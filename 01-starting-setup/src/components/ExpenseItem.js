91180239e5f9094190e16c6eacb6998306e269c1
Alan Leverenz <awleverenz@aol.com>
2023-01-12 11:58:11 -0600

    set props for item and date

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <ExpenseDate />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
