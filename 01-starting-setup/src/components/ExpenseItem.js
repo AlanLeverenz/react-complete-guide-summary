964d9256ce937e95fdece3116264693ef88d22f2
Alan Leverenz <awleverenz@aol.com>
2023-01-11 22:30:37 -0600

    created helper const for month

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -2,8 +2,15 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>Year</div>
+        <div>Date</div>
+      </div>
       <div>{props.Date}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
