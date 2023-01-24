0b8efea586366111702a5ff22ac244032426ab35
Alan Leverenz <awleverenz@aol.com>
2023-01-23 21:26:37 -0500

    added second page

--- 01-starting-project-spa-router/src/App.js
+++ 01-starting-project-spa-router/src/App.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
 
 // https://example.com/products
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
-  {}
+  { path: '/products', element: <ProductsPage /> }
 ]);
 
 function App() {
