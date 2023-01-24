c4edc1876d36751551406003e1c345b1e37c9145
Alan Leverenz <awleverenz@aol.com>
2023-01-23 21:36:26 -0500

    alternate method of setting routes

--- 01-starting-project-spa-router/src/App.js
+++ 01-starting-project-spa-router/src/App.js
@@ -1,8 +1,15 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-// https://example.com/products
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
