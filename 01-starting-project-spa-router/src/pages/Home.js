69ef8ed698ca725739b334d780e7765dc2fb4720
Alan Leverenz <awleverenz@aol.com>
2023-01-24 15:49:23 -0500

    added comments on how Link functions

--- 01-starting-project-spa-router/src/pages/Home.js
+++ 01-starting-project-spa-router/src/pages/Home.js
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
 
 function HomePage() {
+  // Link listens for clicks on that element
+  // prevents browser from reloading all the html
+  // looks at the route definitions
+  // changes the url without sending a new html request
   return (
     <>
       <h1>My Home Page</h1>
