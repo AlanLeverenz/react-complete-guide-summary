cc055e717d43887ad2c7958e4f5d5947850c1fa1
Alan Leverenz <awleverenz@aol.com>
2023-01-25 15:49:30 -0500

    added Error page and set css

--- 01-starting-project-spa-router/src/pages/Root.js
+++ 01-starting-project-spa-router/src/pages/Root.js
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
-import classes from './Root.module.css';
 
 // Outlet is where children of the Root component element appear
 function RootLayout() {
   return <>
     <MainNavigation />
-    <main className={classes.content}>
+    <main>
       <Outlet />
     </main>
 
