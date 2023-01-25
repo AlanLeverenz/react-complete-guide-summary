b3a83244362408efe8660cac2b36a102dc63b9d7
Alan Leverenz <awleverenz@aol.com>
2023-01-25 16:45:59 -0500

    added NavLink to MainNavigation

--- 01-starting-project-spa-router/src/App.js
+++ 01-starting-project-spa-router/src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> }
