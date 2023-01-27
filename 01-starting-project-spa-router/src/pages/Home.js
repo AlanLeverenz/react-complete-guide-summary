2961d73973bec444b64ffdd67566f1541e0d2113
Alan Leverenz <awleverenz@aol.com>
2023-01-26 21:44:53 -0500

    used useNavigate react function

--- 01-starting-project-spa-router/src/pages/Home.js
+++ 01-starting-project-spa-router/src/pages/Home.js
@@ -1,6 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products');
+  }
+
   // Link listens for clicks on that element
   // prevents browser from reloading all the html
   // looks at the route definitions
@@ -10,6 +17,9 @@ function HomePage() {
       <h1>My Home Page</h1>
       <p>
         Go to <Link to="/products">The List of Products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
