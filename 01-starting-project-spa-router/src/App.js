e2512fc61229a7236a99caeaaaae2e4da465e6f7
Alan Leverenz <awleverenz@aol.com>
2023-01-27 11:24:49 -0500

    useParams for ProductDetailsPage

--- 01-starting-project-spa-router/src/App.js
+++ 01-starting-project-spa-router/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail'
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -21,7 +22,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> }
+      { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> }
     ]
   },
 ]);
---
+++ 01-starting-project-spa-router/src/pages/ProductDetail.js
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom'
+
+function ProductDetailPage() {
+  const params = useParams();
+
+
+  return (
+    <>
+      <h1>ProductDetails</h1>
+      <p>{params.productId}</p>
+    </>
+  );
+}
+
+export default ProductDetailPage;
