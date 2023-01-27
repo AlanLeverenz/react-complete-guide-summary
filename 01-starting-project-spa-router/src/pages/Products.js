848c2cdc30034410545abbc8bf196b88045cfdab
Alan Leverenz <awleverenz@aol.com>
2023-01-27 09:17:20 -0500

    added products page

--- 01-starting-project-spa-router/src/pages/Products.js
+++ 01-starting-project-spa-router/src/pages/Products.js
@@ -1,5 +1,14 @@
 function ProductsPage() {
-  return <h1>Products Page</h1>
+  return (
+    <>
+      <h1>Products Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
 }
 
 export default ProductsPage;
