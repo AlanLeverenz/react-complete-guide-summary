c507ffe4e14962d9e493b475aae4e7c5840724d2
Alan Leverenz <awleverenz@aol.com>
2023-02-03 10:39:59 -0500

    added data to event content

--- 12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ 12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,14 +1,18 @@
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom';
+import EventItem from '../components/EventItem';
 
-function EventsDetailPage() {
+function EventsDetailPage(props) {
   const params = useParams();
 
   // relative="route" goes back to parent route (in App)
   // relative="path" moves one segment in the path
+
+  //       <EventItem event={props.event} />
   return (
     <>
       <h1>Event Details</h1>
       <p>{params.eventId}</p>
+
       <p><Link to=".." relative='path'>Back</Link></p>
     </>
   );
--- 12-adv-starting-project/frontend/src/pages/Events.js
+++ 12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,13 +1,10 @@
-// import { Link } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 const EVENTS = [
-  { id: 'e1', title: 'Beatles Revival Concert' },
-  { id: 'e2', title: 'Salsa Dancing at Mercato 60' },
-  { id: 'e3', title: 'New Balance Indoor Track Meet' }
+  { id: 'e1', title: 'Beatles Revival Concert', image: '', date: 'Feb 2, 2023', description: 'Virtual concert with the Fabulous Four' },
+  { id: 'e2', title: 'Salsa Dancing at Mercato 60', image: '', date: 'Feb 3, 2023', description: 'Live salsa music in Merida' },
+  { id: 'e3', title: 'New Balance Indoor Track Meet', image: '', date: 'Feb 4, 2023', description: 'The best track stars in the US' }
 ];
-
-// <EventsList events={EVENTS} />
 
 function Events() {
   return (
