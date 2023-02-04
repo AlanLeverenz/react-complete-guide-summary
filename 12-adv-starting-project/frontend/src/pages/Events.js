9450139a56007c1bb46b4354a4b5542065968709
Alan Leverenz <awleverenz@aol.com>
2023-02-04 12:40:11 -0500

    updated Events page to return loader data from App route function

--- 12-adv-starting-project/frontend/src/pages/Events.js
+++ 12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,37 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+// gets closest loader data
+// useLoaderData gets the result of a promise
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
+  const events = useLoaderData();
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        setError('Fetching events failed.');
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
-  return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-    </>
-  );
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
