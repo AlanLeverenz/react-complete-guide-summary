ceb61785a659ba4c3d40b8a43b655d84f5168f65
Alan Leverenz <awleverenz@aol.com>
2023-02-05 13:07:47 -0500

    adjusted css for event list

--- 12-adv-starting-project/frontend/src/components/EventsList.js
+++ 12-adv-starting-project/frontend/src/components/EventsList.js
@@ -9,10 +9,12 @@ function EventsList({ events }) {
         {events.map((event) => (
           <li key={event.id} className={classes.item}>
             <Link to={event.id}>{event.title}</Link>
-            <img src={event.image} alt={event.title} />
-            <div className={classes.content}>
-              <h2>{event.title}</h2>
-              <time>{event.date}</time>
+            <div className={classes.row}>
+              <img src={event.image} alt={event.title} />
+              <div className={classes.content}>
+                <h2>{event.title}</h2>
+                <time>{event.date}</time>
+              </div>
             </div>
           </li>
         ))}
