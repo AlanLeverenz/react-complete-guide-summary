3d867e74251d98644909d81a7feacad61ad0bd28
Alan Leverenz <awleverenz@aol.com>
2023-02-06 11:02:16 -0500

    moved loader to Events page where it is exported

--- 12-adv-starting-project/frontend/src/App.js
+++ 12-adv-starting-project/frontend/src/App.js
@@ -1,28 +1,6 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage from './pages/EventDetail';
 import EventFormPage from './components/EventForm';
 import EditEventPage from './pages/EditEvent';
@@ -31,6 +9,7 @@ import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
 
 // router knows to provide returned data to its path element
+// imported loader from EventsPage and inserted in route object
 const router = createBrowserRouter([
   {
     path: '/',
@@ -45,16 +24,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-                // ...
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            }
+            loader: eventsLoader,
           },
           { path: ':eventId', element: <EventDetailPage /> },
           { path: 'new', element: <EventFormPage /> },
