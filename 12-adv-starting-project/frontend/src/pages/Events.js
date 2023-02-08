2eb34fd500053d55e4bf532b5e3d7b8f4d70904c
Alan Leverenz <awleverenz@aol.com>
2023-02-08 18:07:31 -0500

    updated Events code to use fetch response for the loader

--- 12-adv-starting-project/frontend/src/pages/Events.js
+++ 12-adv-starting-project/frontend/src/pages/Events.js
@@ -6,21 +6,29 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
-
 export async function loader() {
+  // fetch returns a Response promise
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
+
+
+// const resData = await response.json();
+// // return resData.events;
+
+// // Response function built into the browser, captures a response object. it is client side code
+// const res = new Response('any data', {status: 201});
+// return res;
