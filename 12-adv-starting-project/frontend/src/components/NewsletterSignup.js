f9a1b239efffef769fb5c2ee21ce0f7b29d1e569
Alan Leverenz <awleverenz@aol.com>
2023-02-20 20:10:13 -0500

    useFetcher with NewsletterSignup

--- 12-adv-starting-project/frontend/src/components/NewsletterSignup.js
+++ 12-adv-starting-project/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,34 @@
+import { useEffect } from 'react';
+import { useFetcher } from 'react-router-dom';
+
 import classes from './NewsletterSignup.module.css';
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+
+  // fetcher.Form runs the action for the App route (behind the scenes)
+  // but does not transition to another route
+
+  useEffect(() => {
+    if (state === 'idle' && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
