5331e83ecbb6200d2f446ed02e3a70bbdfce827e
Alan Leverenz <awleverenz@aol.com>
2023-02-22 15:10:31 -0500

    applied defer, await, suspense to events

--- 12-adv-starting-project/frontend/src/pages/Events.js
+++ 12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,47 +1,44 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 
-// gets closest loader data
 // useLoaderData gets the result of a promise
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  // if (data.isError) {
-  //   return <p>{data.message}</p>
-  // }
-
-  const events = data.events;
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading ...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  )
 }
 
 export default EventsPage;
 
-export async function loader() {
-  // fetch returns a Response promise
-  // cannot useState in loader functions
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // return { isError: true, message: 'Could not fetch events.' };
-    // throw new Response(JSON.stringify({ message: 'Could not fetch events' }), { status: 500, });
-    return json(
+    throw json(
       { message: 'Could not fetch events' },
       {
         status: 500,
       }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
 }
 
+export function loader() {
+  // defer returns the events after page is loaded
+  return defer({
+    events: loadEvents()
+  })
 
-// const resData = await response.json();
-// // return resData.events;
-
-// // Response function built into the browser, captures a response object. it is client side code
-// const res = new Response('any data', {status: 201});
-// return res;
+}
