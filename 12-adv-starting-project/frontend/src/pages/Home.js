5f42f33be85d4d3ebc850e01b4ee5318ff8bf076
Alan Leverenz <awleverenz@aol.com>
2023-02-24 14:19:42 -0500

    added HomePage content

--- 12-adv-starting-project/frontend/src/pages/Home.js
+++ 12-adv-starting-project/frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
+import PageContent from '../components/PageContent';
+
 function HomePage() {
 
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events</p>
+    </PageContent>
   );
 }
 
