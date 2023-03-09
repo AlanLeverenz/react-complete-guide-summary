7fedf349734313f1135dc0289b833ba30dc96928
Alan Leverenz <awleverenz@aol.com>
2023-03-08 21:26:21 -0500

    added timer for logout

--- 01-starting-project-auth/frontend/src/pages/Root.js
+++ 01-starting-project-auth/frontend/src/pages/Root.js
@@ -1,9 +1,24 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  // to get the token
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  // useEffect runs whenever token changes
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    // if there is a token, set timeout to 1 hour
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' })
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
