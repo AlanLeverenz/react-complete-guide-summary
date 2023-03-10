415242fd4be01a2596209bcf5d824f018f502e3b
Alan Leverenz <awleverenz@aol.com>
2023-03-09 22:16:34 -0500

    added expiration time to match server

--- 01-starting-project-auth/frontend/src/pages/Authentication.js
+++ 01-starting-project-auth/frontend/src/pages/Authentication.js
@@ -41,7 +41,11 @@ export async function action({ request }) {
   const resData = await response.json();
   const token = resData.token;
 
+  // need to set an expiration time that matches the server
   localStorage.setItem('token', token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem('expiration', expiration.toISOString());
 
   return redirect('/');
 }
