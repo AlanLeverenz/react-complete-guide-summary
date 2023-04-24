5bf0a582fe6ec2edc9c4a534935d942e1f88e4cb
Alan Leverenz <awleverenz@aol.com>
2023-04-23 23:16:58 -0400

    inserted module css in Post

--- starting-project-react-basics/src/components/Post.jsx
+++ starting-project-react-basics/src/components/Post.jsx
@@ -1,9 +1,11 @@
+import classes from './Post.module.css';
+
 function Post(props) {
 
   return (
-    <div>
-      <p>{props.author}</p>
-      <p>{props.body}</p>
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
     </div>
   )
 }
