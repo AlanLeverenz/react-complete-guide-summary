2d3fc0d3a58c0a77673cb0f3778a9bdd2ad8885f
Alan Leverenz <awleverenz@aol.com>
2023-04-27 17:49:08 -0400

    applied useState to update form value

--- starting-project-react-basics/src/components/NewPost.jsx
+++ starting-project-react-basics/src/components/NewPost.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState('');
 
   function changeBodyHandler(event) {
-    console.log(event.target.value);
+    setEnteredBody(event.target.value);
   }
 
   return (
@@ -12,6 +15,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
