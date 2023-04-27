414dc5de110b943b6bb2006f90e8534d4e41483e
Alan Leverenz <awleverenz@aol.com>
2023-04-27 18:10:29 -0400

    lifted state to higher component to pass changehandler functions

--- starting-project-react-basics/src/components/NewPost.jsx
+++ starting-project-react-basics/src/components/NewPost.jsx
@@ -1,24 +1,15 @@
-import { useState } from 'react';
-
 import classes from './NewPost.module.css';
 
-function NewPost() {
-  const [enteredBody, setEnteredBody] = useState('');
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- starting-project-react-basics/src/components/PostsList.jsx
+++ starting-project-react-basics/src/components/PostsList.jsx
@@ -1,45 +1,33 @@
+import { useState } from 'react';
+
 import classes from './PostsList.module.css';
 import Post from './Post';
 import NewPost from './NewPost';
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler} />
       <ul className={classes.posts}>
-        <Post author="Maximilian" body="React.js is awesome" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Checkout the full course!" />
-        <Post author="Kimberley" body="The novelist in Norway" />
+        <Post author="Kimberley" body="The best novelist in Norway" />
       </ul>
     </>
   )
 }
 
 export default PostsList;
-
-// const posts = [
-//   {
-//     author: "Maximilian",
-//     body: "React.js is awesome!"
-//   },
-//   {
-//     author: "Manuel",
-//     body: "Check out the full course!"
-//   },
-//   {
-//     author: "Kimberly",
-//     body: "The best novel about Norway!"
-//   }
-// ];
-
-// <ul className={classes.posts}>
-// {props.map(post => (
-//   <li>
-//     <Post
-//       key={post.author}
-//       author={post.author}
-//       body={post.body}
-//     />
-//   </li>
-// ))}
-// </ul>
