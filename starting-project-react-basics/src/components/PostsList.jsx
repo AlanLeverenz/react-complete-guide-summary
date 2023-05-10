e9893c84c79f6990a0bb1cc80f41c445afa89aad
Alan Leverenz <awleverenz@aol.com>
2023-05-10 09:11:12 -0400

    reset code for App and PostsList

--- starting-project-react-basics/src/components/PostsList.jsx
+++ starting-project-react-basics/src/components/PostsList.jsx
@@ -7,11 +7,25 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
