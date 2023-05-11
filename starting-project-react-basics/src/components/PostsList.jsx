efd3b8fb1607dbb07988b75df32a7266a41b9ff7
Alan Leverenz <awleverenz@aol.com>
2023-05-11 16:35:03 -0400

    set conditionals for displaying posts or a message if none

--- starting-project-react-basics/src/components/PostsList.jsx
+++ starting-project-react-basics/src/components/PostsList.jsx
@@ -22,9 +22,19 @@ function PostsList({ isPosting, onStopPosting }) {
             onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
