1f3814672edc0eee9a10dcfe6977ffbc373b33db
Alan Leverenz <awleverenz@aol.com>
2023-05-17 12:28:08 -0400

    added MainHeader to RootLayout

--- starting-project-react-basics/src/main.jsx
+++ starting-project-react-basics/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NewPost from './components/NewPost';
 
 import App from './App';
 import './index.css';
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> }, // <our-domain/>,
-
+  { path: '/create-post', element: <NewPost /> }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
