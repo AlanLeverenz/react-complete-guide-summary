2cf1afe119e49ef7d31713ab6909e4365037ae42
Alan Leverenz <awleverenz@aol.com>
2023-05-17 12:33:51 -0400

    refactored Posts and main

--- starting-project-react-basics/src/main.jsx
+++ starting-project-react-basics/src/main.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import NewPost from './components/NewPost';
 
-import App from './App';
+import App from './routes/Posts';
+import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout';
 import './index.css';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> }, // <our-domain/>,
-  { path: '/create-post', element: <NewPost /> }
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> }
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ starting-project-react-basics/src/routes/Posts.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import PostsList from '../components/PostsList';
+
+function Posts() {
+
+  return (
+    <>
+      <main>
+        <PostsList />
+      </main>
+    </>
+  );
+}
+
+export default Posts;
