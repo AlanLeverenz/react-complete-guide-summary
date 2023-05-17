08db33631c23e6853e08e43042ed05a2b19e3fbd
Alan Leverenz <awleverenz@aol.com>
2023-05-17 12:15:04 -0400

    updated App with createBrowserRouter constant

--- starting-project-react-basics/src/main.jsx
+++ starting-project-react-basics/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import './index.css';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> }, // <our-domain/>,
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
