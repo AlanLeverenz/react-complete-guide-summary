c20a1004580b125d19bcb54dc442e4c1895c3d3b
Alan Leverenz <awleverenz@aol.com>
2023-05-19 18:15:03 -0400

    used useLoaderData to fetch data from main route

--- starting-project-react-basics/src/components/PostsList.jsx
+++ starting-project-react-basics/src/components/PostsList.jsx
@@ -1,25 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch('http://localhost:8080/posts')
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
-
-  // will only execute once after the component renders if there is an empty dependencies array (used for functions/values outside of the useEffect function)
+  const posts = useLoaderData();
 
   function addPostHandler(postData) {
     fetch('http://locahost:8080/posts', {
@@ -34,24 +19,19 @@ function PostsList() {
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet</h2>
           <p>Start adding some!</p>
         </div>
       )}
-      {isFetching &&
-        (<div style={{ textAlign: 'center', color: 'white' }}>
-          <p>Loading posts...</p>
-        </div>)
-      }
     </>
   );
 }
