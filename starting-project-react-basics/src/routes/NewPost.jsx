6ba7848fcf288f67ebcdc1d4937ce7dfb694cd04
Alan Leverenz <awleverenz@aol.com>
2023-05-22 11:25:41 -0400

    refactored NewPost and main for action

--- starting-project-react-basics/src/routes/NewPost.jsx
+++ starting-project-react-basics/src/routes/NewPost.jsx
@@ -1,48 +1,30 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Form } from 'react-router-dom';
 
 import classes from './NewPost.module.css';
 import Modal from '../components/Modal';
 
 function NewPost() {
 
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  function submitHandler(event) {
-    event.preventDefault();
-    const postData = {
-      body: enteredBody,
-      author: enteredAuthor
-    };
-    fetch('http://locahost:8080/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-  }
-
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <Form method="post" className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+          <textarea
+            id="body"
+            name="body"
+            required
+            rows={3}
+          />
         </p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input type="text" id="name" required onChange={authorChangeHandler} />
+          <input
+            type="text"
+            id="name"
+            name="author"
+            required
+          />
         </p>
         <p className={classes.actions}>
           <Link to=".." type="button">
@@ -50,9 +32,19 @@ function NewPost() {
           </Link>
           <button>Submit</button>
         </p>
-      </form>
+      </Form>
     </Modal>
   );
 }
 
 export default NewPost;
+
+export function action() {
+  fetch('http://locahost:8080/posts', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
