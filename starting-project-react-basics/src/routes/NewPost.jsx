d5fc507e71160c29c626b541ef43fcf8007d2eb3
Alan Leverenz <awleverenz@aol.com>
2023-05-22 11:42:21 -0400

    corrected NewPost code

--- starting-project-react-basics/src/routes/NewPost.jsx
+++ starting-project-react-basics/src/routes/NewPost.jsx
@@ -1,30 +1,19 @@
-import { Link, Form } from 'react-router-dom';
+import { Link, Form, redirect } from 'react-router-dom';
 
 import classes from './NewPost.module.css';
 import Modal from '../components/Modal';
 
 function NewPost() {
-
   return (
     <Modal>
-      <Form method="post" className={classes.form}>
+      <Form method='post' className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea
-            id="body"
-            name="body"
-            required
-            rows={3}
-          />
+          <textarea id="body" name="body" required rows={3} />
         </p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input
-            type="text"
-            id="name"
-            name="author"
-            required
-          />
+          <input type="text" id="name" name="author" required />
         </p>
         <p className={classes.actions}>
           <Link to=".." type="button">
@@ -39,17 +28,16 @@ function NewPost() {
 
 export default NewPost;
 
-// request' is the object that is requested by the action
-// Form function handles form functions within react on client side
-
 export async function action({ request }) {
   const formData = await request.formData();
-  const postData = Object.fromEntries(formData); // { body: '...', author: '...'}
-  fetch('http://locahost:8080/posts', {
+  const postData = Object.fromEntries(formData); // { body: '...', author: '...' }
+  await fetch('http://localhost:8080/posts', {
     method: 'POST',
     body: JSON.stringify(postData),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   });
+
+  return redirect('/');
 }
