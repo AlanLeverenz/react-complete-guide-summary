ea0aa68b053e7cd1659f1663b21b62ff4ea9d76a
Alan Leverenz <awleverenz@aol.com>
2023-05-27 22:20:43 -0400

    added css to ExpenseItem

--- 01-starting-setup-basics/src/components/ExpenseItem.js
+++ 01-starting-setup-basics/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className='expense-item'>
+      <div>March 28th 2023</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$249.67</div>
       </div>
     </div>
   );
