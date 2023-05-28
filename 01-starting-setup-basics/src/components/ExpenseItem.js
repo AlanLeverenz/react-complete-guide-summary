b8af88e995a0858601cd58a5b30b0eb8f637ee8b
Alan Leverenz <awleverenz@aol.com>
2023-05-28 10:50:12 -0400

    updated ExpenseItem with additional items

--- 01-starting-setup-basics/src/components/ExpenseItem.js
+++ 01-starting-setup-basics/src/components/ExpenseItem.js
@@ -2,13 +2,29 @@ import './ExpenseItem.css';
 
 function ExpenseItem() {
   return (
-    <div className='expense-item'>
-      <div>March 28th 2023</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$249.67</div>
+    <>
+      <div className='expense-item'>
+        <div>March 28th 2023</div>
+        <div className='expense-item__description'>
+          <h2>Car Insurance</h2>
+          <div className='expense-item__price'>$249.67</div>
+        </div>
       </div>
-    </div>
+      <div className='expense-item'>
+        <div>March 28th 2023</div>
+        <div className='expense-item__description'>
+          <h2>Car Insurance</h2>
+          <div className='expense-item__price'>$249.67</div>
+        </div>
+      </div>
+      <div className='expense-item'>
+        <div>March 28th 2023</div>
+        <div className='expense-item__description'>
+          <h2>Car Insurance</h2>
+          <div className='expense-item__price'>$249.67</div>
+        </div>
+      </div>
+    </>
   );
 }
 
