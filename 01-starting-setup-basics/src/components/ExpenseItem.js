2553da2f735a1b11a163c2c8acd9aca7f17dd030
Alan Leverenz <awleverenz@aol.com>
2023-05-29 07:28:51 -0400

    replaced hard-coded values with consts

--- 01-starting-setup-basics/src/components/ExpenseItem.js
+++ 01-starting-setup-basics/src/components/ExpenseItem.js
@@ -1,27 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <>
       <div className='expense-item'>
-        <div>March 28th 2023</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className='expense-item__description'>
-          <h2>Car Insurance</h2>
-          <div className='expense-item__price'>$249.67</div>
-        </div>
-      </div>
-      <div className='expense-item'>
-        <div>March 28th 2023</div>
-        <div className='expense-item__description'>
-          <h2>Car Insurance</h2>
-          <div className='expense-item__price'>$249.67</div>
-        </div>
-      </div>
-      <div className='expense-item'>
-        <div>March 28th 2023</div>
-        <div className='expense-item__description'>
-          <h2>Car Insurance</h2>
-          <div className='expense-item__price'>$249.67</div>
+          <h2>{expenseTitle}</h2>
+          <div className='expense-item__price'>${expenseAmount}</div>
         </div>
       </div>
     </>
