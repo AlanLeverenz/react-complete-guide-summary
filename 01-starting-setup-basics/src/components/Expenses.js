9b9f771a4abe7bc4ee567339ecac7a98c82b6708
Alan Leverenz <awleverenz@aol.com>
2023-05-31 22:07:53 -0400

    edited Expenses to get correct item displayed

--- 01-starting-setup-basics/src/components/Expenses.js
+++ 01-starting-setup-basics/src/components/Expenses.js
@@ -1,31 +1,33 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 
-function Expenses(expenses) {
+function Expenses(props) {
   return (
     <div className='expenses'>
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
       />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+
     </div>
   )
 }
 
 export default Expenses;
+
+// <ExpenseItem
+// title={expenses[1].title}
+// amount={expenses[1].amount}
+// date={expenses[1].date}
+// />
+// <ExpenseItem
+// title={expenses[2].title}
+// amount={expenses[2].amount}
+// date={expenses[2].date}
+// />
+// <ExpenseItem
+// title={expenses[3].title}
+// amount={expenses[3].amount}
+// date={expenses[3].date}
+// />
