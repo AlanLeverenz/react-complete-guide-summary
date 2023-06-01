917b7346b8ad09aab869defeee8ed0d197d6ee6d
Alan Leverenz <awleverenz@aol.com>
2023-05-31 22:09:26 -0400

    corrected Expenses.js

--- 01-starting-setup-basics/src/components/Expenses.js
+++ 01-starting-setup-basics/src/components/Expenses.js
@@ -9,25 +9,24 @@ function Expenses(props) {
         amount={props.expenses[0].amount}
         date={props.expenses[0].date}
       />
-
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+      <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
     </div>
   )
 }
 
 export default Expenses;
 
-// <ExpenseItem
-// title={expenses[1].title}
-// amount={expenses[1].amount}
-// date={expenses[1].date}
-// />
-// <ExpenseItem
-// title={expenses[2].title}
-// amount={expenses[2].amount}
-// date={expenses[2].date}
-// />
-// <ExpenseItem
-// title={expenses[3].title}
-// amount={expenses[3].amount}
-// date={expenses[3].date}
-// />
