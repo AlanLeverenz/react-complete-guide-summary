39f711f6d2d8d8b13ff5a3227b8c16d2a2fa2c2d
Alan Leverenz <awleverenz@aol.com>
2023-06-07 19:43:46 -0400

    edited ExpenseItem to setup state

--- 01-starting-setup-basics/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup-basics/src/components/Expenses/ExpenseItem.js
@@ -4,8 +4,12 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log('Clicked!!!');
+    title = 'Updated';
+    console.log({ title });
   };
 
   return (
@@ -13,7 +17,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate
         date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
