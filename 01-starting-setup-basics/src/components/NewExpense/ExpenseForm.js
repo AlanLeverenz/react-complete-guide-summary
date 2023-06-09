168796aa8ddd5dee650a877835d44145f80056b5
Alan Leverenz <awleverenz@aol.com>
2023-06-09 19:06:21 -0400

    set state for all form inputs

--- 01-starting-setup-basics/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup-basics/src/components/NewExpense/ExpenseForm.js
@@ -4,9 +4,19 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
 
 
@@ -19,11 +29,11 @@ const ExpenseForm = () => {
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min="0.01" step="0.01" />
+          <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='number' min="2019-01-01" max="2023-12-31" />
+          <input type='number' min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
