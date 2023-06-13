323edb3389f108d94352ec498dc71b9099356167
Alan Leverenz <awleverenz@aol.com>
2023-06-13 17:06:38 -0400

    fixed date format

--- 01-starting-setup-basics/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup-basics/src/components/NewExpense/ExpenseForm.js
@@ -72,7 +72,7 @@ const ExpenseForm = () => {
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='number' min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+          <input type='date' min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
