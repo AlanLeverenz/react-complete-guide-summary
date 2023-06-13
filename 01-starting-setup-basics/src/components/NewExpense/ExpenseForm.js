b0084c0c74715871e83d6d28fe89eb4d1913d39b
Alan Leverenz <awleverenz@aol.com>
2023-06-13 17:13:28 -0400

    cleared form data after submitting

--- 01-starting-setup-basics/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup-basics/src/components/NewExpense/ExpenseForm.js
@@ -57,6 +57,9 @@ const ExpenseForm = () => {
       date: new Date(enteredDate)
     };
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -64,15 +67,28 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+          <input
+            type='number'
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler} />
+          <input
+            type='date'
+            min="2019-01-01"
+            max="2023-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
