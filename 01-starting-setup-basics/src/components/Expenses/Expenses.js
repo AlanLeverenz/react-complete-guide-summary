2ee916678be5195a947aa4be4aa02b4af0f992d2
Alan Leverenz <awleverenz@aol.com>
2023-06-16 21:45:19 -0400

    added onChange function to Expenses

--- 01-starting-setup-basics/src/components/Expenses/Expenses.js
+++ 01-starting-setup-basics/src/components/Expenses/Expenses.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {
+
+  // const [filteredYear, setFilteredYear] = useState('2021');
+
+  const filterChangeHandler = (selectedYear) => {
+    // setFilteredYear(selectedYear);
+    console.log('Expenses.js');
+    console.log(selectedYear);
+  }
+
+  // const filteredExpenses = props.items.filter(expense => {
+  //   return expense.date.getFullYear().toString() === filteredYear;
+
+  // });
+
   return (
-    <Card className='expenses'>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <div>
+
+      <Card className='expenses'>
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   )
 }
 
--- 01-starting-setup-basics/src/components/Expenses/ExpensesFilter.js
+++ 01-starting-setup-basics/src/components/Expenses/ExpensesFilter.js
@@ -4,7 +4,8 @@ import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
   const dropdownChangeHandler = (event) => {
-    props.onChangeFilter(event.target.value);
+    // props.onChangeFilter(event.target.value);
+    console.log(event.target.value);
   };
 
   return (
