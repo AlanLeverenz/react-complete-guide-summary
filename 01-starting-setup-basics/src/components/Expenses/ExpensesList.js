e848ba472c08b9a3b633f18c1680f02e4eab3c08
Alan Leverenz <awleverenz@aol.com>
2023-07-02 11:40:55 -0400

    refactored ExpensesList for returning expense items

--- 01-starting-setup-basics/src/components/Expenses/ExpensesList.js
+++ 01-starting-setup-basics/src/components/Expenses/ExpensesList.js
@@ -5,22 +5,22 @@ import ExpenseItem from './ExpenseItem';
 
 const ExpensesList = props => {
 
-  // can store JSX content in variables
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (props.items.length > 0) {
-    expensesContent = props.items.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>
   }
 
-  return <ul className="expenses-list">{expensesContent}</ul>
-
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  )
 }
 
 export default ExpensesList
