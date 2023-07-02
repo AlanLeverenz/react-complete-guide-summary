2c51d5fe0b013cbc91e108393da5704b57548b7a
Alan Leverenz <awleverenz@aol.com>
2023-07-02 11:34:42 -0400

    inserted ExpensesList in Expenses

--- 01-starting-setup-basics/src/components/Expenses/ExpensesList.js
+++ 01-starting-setup-basics/src/components/Expenses/ExpensesList.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import './ExpensesList.css';
+import ExpenseItem from './ExpenseItem';
+
 const ExpensesList = props => {
 
+  // can store JSX content in variables
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (props.items.length > 0) {
+    expensesContent = props.items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
+  return <ul className="expenses-list">{expensesContent}</ul>
 
 }
 
