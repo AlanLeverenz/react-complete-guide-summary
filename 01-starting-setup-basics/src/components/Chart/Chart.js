858871434c6ee76c4af2008abe05955ae8df927b
Alan Leverenz <awleverenz@aol.com>
2023-07-04 22:48:22 -0400

    added key to map

--- 01-starting-setup-basics/src/components/Chart/Chart.js
+++ 01-starting-setup-basics/src/components/Chart/Chart.js
@@ -7,6 +7,7 @@ const Chart = props => {
   return <div className="chart">
     {props.dataPoints.map(dataPoint => (
       <ChartBar
+        key={dataPoint.label}
         value={dataPoint.value}
         maxValue={null}
         label={dataPoint.label}
