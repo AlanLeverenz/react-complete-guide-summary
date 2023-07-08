e81eb6d689725e7e911e89a974b891358f360f83
Alan Leverenz <awleverenz@aol.com>
2023-07-07 21:45:58 -0400

    edited Chart code to obtain totalMaximum value

--- 01-starting-setup-basics/src/components/Chart/Chart.js
+++ 01-starting-setup-basics/src/components/Chart/Chart.js
@@ -4,16 +4,22 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = props => {
-  return <div className="chart">
-    {props.dataPoints.map(dataPoint => (
-      <ChartBar
-        key={dataPoint.label}
-        value={dataPoint.value}
-        maxValue={null}
-        label={dataPoint.label}
-      />
-    ))}
-  </div>
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  // spread operator creates separate arguments of an array
+  const totalMaximum = Math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map(dataPoint => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  )
 };
 
 export default Chart;
