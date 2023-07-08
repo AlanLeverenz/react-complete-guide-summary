104de89f84217b9b740822479ab4a825e0e246cd
Alan Leverenz <awleverenz@aol.com>
2023-07-07 21:55:09 -0400

    corrections

--- 01-starting-setup-basics/src/components/Chart/ChartBar.js
+++ 01-starting-setup-basics/src/components/Chart/ChartBar.js
@@ -7,7 +7,7 @@ const ChartBar = props => {
   let barFillHeight = '0%';
 
   // convert to string for css to process
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
   }
 
--- 01-starting-setup-basics/src/components/Expenses/Expenses.js
+++ 01-starting-setup-basics/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './Expenses.css';
 import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {
@@ -26,6 +27,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
