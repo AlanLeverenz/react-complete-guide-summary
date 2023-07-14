221b9171102a6b66c439994434138a5e477d94bb
Alan Leverenz <awleverenz@aol.com>
2023-07-14 17:20:58 -0400

    reset CourseInput code for dynamic classes

--- 01-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,13 +26,8 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{
-          color: !isValid ? 'red' : 'black'
-        }}>Course Goal</label>
-        <input style={{
-          borderColor: !isValid ? 'red' : '#ccc',
-          background: !isValid ? 'salmon' : 'transparent'
-        }} type="text" onChange={goalInputChangeHandler} />
+        <label >Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
