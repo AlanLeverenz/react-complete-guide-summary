86bce64bfc71cadd074a5a5612977edd5a835c92
Alan Leverenz <awleverenz@aol.com>
2023-10-05 16:38:09 -0400

    edited ResultsTable and imported into App

--- 01-starting-project-invest-calc/src/App.js
+++ 01-starting-project-invest-calc/src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header/header';
 import UserInput from './components/UserInput/UserInput';
+import ResultsTable from './components/ResultsTable/ResultsTable';
 
 function App() {
 
@@ -36,30 +37,8 @@ function App() {
 
       <UserInput />
 
+      <ResultsTable />
 
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   );
 }
