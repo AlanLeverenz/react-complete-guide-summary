1b05efe7d7b24dae4bb9ef38406389ace75255e3
Alan Leverenz <awleverenz@aol.com>
2023-10-09 14:12:57 -0400

    added css cmodule for Results Table

--- 01-starting-project-invest-calc/src/components/ResultsTable/ResultsTable.js
+++ 01-starting-project-invest-calc/src/components/ResultsTable/ResultsTable.js
@@ -1,6 +1,15 @@
+import classes from './ResultsTable.module.css';
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
 const ResultsTable = (props) => {
   return (
-    <table className="result">
+    <table className={classes.result}>
       <thead>
         <tr>
           <th>Year</th>
@@ -12,23 +21,28 @@ const ResultsTable = (props) => {
       </thead>
       <tbody>
         {props.data.map((yearData) => (
-          <tr>
+          <tr key={yearData.year}>
             <td>{yearData.year}</td>
-            <td>{yearData.savingsEndOfYear}</td>
-            <td>{yearData.yearlyInterest}</td>
-            <td>{yearData.savingsEndOfYear -
-              props.initialInvestment -
-              yearData.yearlyContribution * yearData.year}
+            <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+            <td>{formatter.format(yearData.yearlyInterest)}</td>
+            <td>
+              {formatter.format(
+                yearData.savingsEndOfYear -
+                props.initialInvestment -
+                yearData.yearlyContribution * yearData.year
+              )}
             </td>
-            <td>{props.initialInvestment +
-              yearData.yearlyContribution *
-              yearData.year}
+            <td>
+              {formatter.format(
+                props.initialInvestment +
+                yearData.yearlyContribution * yearData.year
+              )}
             </td>
           </tr>
-        ))};
+        ))}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
 export default ResultsTable;
