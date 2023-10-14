db3ef84560dc5b8b1c68df26cf39532063997ac0
Alan Leverenz <awleverenz@aol.com>
2023-10-14 15:15:53 -0400

    attempted to convert codeshare into xcode

--- react-vs-vanilla-demo/src/index.js
+++ react-vs-vanilla-demo/src/index.js
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import React from 'react';
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
@@ -11,3 +12,7 @@ root.render(
     <App />
   </StrictMode>
 );
+
+// root.render(
+//   <App />
+// );
