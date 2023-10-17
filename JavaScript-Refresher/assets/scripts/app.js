6279980365125af7b26c6667826f74ec6dd5a24e
Alan Leverenz <awleverenz@aol.com>
2023-10-16 21:57:23 -0400

    sampled more import methods

--- JavaScript-Refresher/assets/scripts/app.js
+++ JavaScript-Refresher/assets/scripts/app.js
@@ -1,4 +1,8 @@
 // import { apiKey } from './util.js';
-import apiKey from './util.js';
+// import apiKey from './util.js';
+import { abc as content } from './util.js';
 
-console.log(apiKey);
+// import * as util from './util.js';
+
+// console.log(util.abc);
+console.log(content);
--- JavaScript-Refresher/assets/scripts/util.js
+++ JavaScript-Refresher/assets/scripts/util.js
@@ -1,2 +1,4 @@
 // export let apiKey = 'asdfgh';
 export default 'asdfgh';
+export let apiKey = 'asdfgh';
+export let abc = 'abc';
