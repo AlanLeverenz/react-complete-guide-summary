d792cdca70dfbfbcacdf427fe35d047d300e86ff
Alan Leverenz <awleverenz@aol.com>
2024-01-23 15:32:53 -0500

    updated app function with component props

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-
+import componentsImg from './assets/components.png';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -9,7 +9,6 @@ function genRandomInt(max) {
 }
 
 function Header() {
-
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
@@ -22,14 +21,14 @@ function Header() {
   )
 }
 
-function CoreConcept() {
+function CoreConcept(props) {
   return (
     <li>
-      <img src="..." alt="..." />
-      <h3>TITLE</h3>
-      <p>DESCRIPTION</p>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
     </li>
-  )
+  );
 }
 
 function App() {
@@ -37,7 +36,17 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+          </ul>
+        </section>
+
       </main>
     </div>
   );
