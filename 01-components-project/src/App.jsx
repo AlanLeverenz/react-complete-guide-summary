42247b1ba883fe88803e0a8b31d307559b9c1965
Alan Leverenz <awleverenz@aol.com>
2024-01-24 22:07:19 -0500

    mixed the methods for displaying each JSX object

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -41,7 +41,11 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
 
-            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
