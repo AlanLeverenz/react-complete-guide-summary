d3aacffe38a4e9086256dd1413aab57113f39761
Alan Leverenz <awleverenz@aol.com>
2024-01-24 22:09:55 -0500

    used object destructuring

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -22,12 +22,13 @@ function Header() {
   )
 }
 
-function CoreConcept(props) {
+// object destructuring
+function CoreConcept({ image, title, description }) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
