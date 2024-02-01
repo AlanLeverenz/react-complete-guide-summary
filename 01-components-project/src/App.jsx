f774f125ccc3825e758b3e1343d55e737d70650c
Alan Leverenz <awleverenz@aol.com>
2024-01-31 21:50:22 -0500

    added identifers for function displaying content

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -4,10 +4,10 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  function handleSelect() {
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components, 'jsx', 'props', 'state'
     console.log('Hello World - selected!');
   }
-
 
   return (
     <div>
@@ -31,11 +31,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+
 
 
         </section>
