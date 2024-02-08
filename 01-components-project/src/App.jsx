bea663259ba38c64e9b8e60228364b2185884d0c
Alan Leverenz <awleverenz@aol.com>
2024-02-08 18:37:44 -0500

    added the key prop

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
           <h2>Core Concepts</h2>
 
           <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept {...conceptItem} />)}
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
           </ul>
         </section>
         <section id="examples">
