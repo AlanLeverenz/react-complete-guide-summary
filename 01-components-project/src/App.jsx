cc1cd1bd319979ae3393ae77db5ac9301b80cb8a
Alan Leverenz <awleverenz@aol.com>
2024-02-13 12:24:44 -0500

    cleanup App

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -3,7 +3,6 @@ import CoreConcepts from './components/CoreConcepts.jsx';
 import Examples from './components/Examples.jsx';
 
 function App() {
-
   return (
     <>
       <Header />
@@ -16,12 +15,3 @@ function App() {
 }
 
 export default App;
-
-// <CoreConcept
-// title={CORE_CONCEPTS[0].title}
-// description={CORE_CONCEPTS[0].description}
-// image={CORE_CONCEPTS[0].image}
-// />
-// <CoreConcept {...CORE_CONCEPTS[1]} />
-// <CoreConcept {...CORE_CONCEPTS[2]} />
-// <CoreConcept {...CORE_CONCEPTS[3]} />
