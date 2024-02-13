b61d949f5a3babe5ff416d1a2a9cdb0eb06a192c
Alan Leverenz <awleverenz@aol.com>
2024-02-13 12:07:49 -0500

    added CoreConcepts component file

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -32,13 +32,7 @@ function App() {
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
 
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
-          </ul>
-        </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
---
+++ 01-components-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,12 @@
+import CoreConcept from './CoreConcept.jsx';
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+      </ul>
+    </section>
+  );
+}
