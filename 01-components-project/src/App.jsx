194c5817da74e87e238f67caa01ddd8d63b67063
Alan Leverenz <awleverenz@aol.com>
2024-02-12 23:34:06 -0500

    used Fragment to avoid unnecessary div

--- 01-components-project/src/App.jsx
+++ 01-components-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
@@ -29,7 +29,7 @@ function App() {
   }
 
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <section id="core-concepts">
@@ -60,7 +60,7 @@ function App() {
 
         </section>
       </main>
-    </div>
+    </Fragment>
   );
 }
 
