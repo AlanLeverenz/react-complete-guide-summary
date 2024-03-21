e1902abf3d932d199b2a7dc85a0483a96ce35360
Alan Leverenz <awleverenz@aol.com>
2024-03-20 21:57:59 -0400

    initial app setup

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,9 +2,23 @@
 function App() {
 
 
-  return (
-    <p>Coming soon...</p>
-  )
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>
 }
 
 export default App
