20164946a0a71d504f0e11a78a5c8649c5e19860
Alan Leverenz <awleverenz@aol.com>
2024-03-22 23:17:24 -0400

    added onclick function

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,13 +3,17 @@ import { useState } from 'react';
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span classNAme="player">
         <span className="player-name">{name}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   )
 }
