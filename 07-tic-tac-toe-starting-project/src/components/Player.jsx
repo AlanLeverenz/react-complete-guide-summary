f4aa1995626345b1770f81b6c82c99a433a87a94
Alan Leverenz <awleverenz@aol.com>
2024-03-23 22:38:18 -0400

    used function form for handling state changes

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,7 +4,7 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(isEditing ? false : true);
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>
