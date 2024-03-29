0684927c313c7e952f83aa55daefcc01298e65e6
Alan Leverenz <awleverenz@aol.com>
2024-03-28 22:12:38 -0400

    added mapping for row and cols

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,12 +1,17 @@
 const initialGameBoard = [
-  [],
-  [],
-  []
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
 ];
 
 export default function GameBoard() {
   return <ol id="game-board">
-    {initialGameBoard}
-  </ol>
+    {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+      <ol>
+        {row.map(col, colIndex => <li key={colIndex}><button></button></li>)}
+      </ol>
+
+    </li>)}
+  </ol>;
 
 }
