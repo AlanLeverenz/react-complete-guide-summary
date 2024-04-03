a1ba0210d18613f4f4b143ed142308e47f682ed2
Alan Leverenz <awleverenz@aol.com>
2024-04-03 15:50:50 -0400

    updated GameBoard

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,10 +7,24 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // need a copy of the array so the original isn't updated out of sequence for update events
+  // this makes the data immutable before updating
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    setGameBoard((prevGameBoard) => {
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return prevGameBoard;
+    });
+  }
+
   return <ol id="game-board">
     {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
       <ol>
-        {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
+        {row.map((playerSymbol, colIndex) => <li key={colIndex}>
+          <button onClick={handleSelectSquare}>{playerSymbol}</button></li>)}
       </ol>
 
     </li>)}
