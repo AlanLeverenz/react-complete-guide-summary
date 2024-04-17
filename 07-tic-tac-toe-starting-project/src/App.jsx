b5831d3ddf17ac1c60c3589a83271838a5d9b78a
Alan Leverenz <awleverenz@aol.com>
2024-04-17 13:37:00 -0400

    updated App

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -27,10 +27,20 @@ function App() {
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+        <Player
+          initialName="Player 1"
+          symbol="X"
+          isActive={activePlayer === 'X'}
+        />
+        <Player
+          initialName="Player 2"
+          symbol="O"
+          isActive={activePlayer === 'O'}
+        />
       </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+      <GameBoard
+        onSelectSquare={handleSelectSquare}
+        turns={gameTurns} />
     </div>
 
     LOG
